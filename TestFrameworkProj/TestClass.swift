0e87106e6ec07a413952fbd7a2d000c6481be65f
PrasadPotale <prasadpotale@gmail.com>
2016-11-30 20:46:57 +0530

    first commit

---
+++ TestFrameworkProj/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  TestFrameworkProj
+//
+//  Created by KahunaOSx on 11/30/16.
+//  Copyright © 2016 Kahuna Systems Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+    func printMessage(){
+        print("My Message")
+    }
+}
---
+++ TestFrameworkProj/TestFrameworkProj.h
@@ -0,0 +1,19 @@
+//
+//  TestFrameworkProj.h
+//  TestFrameworkProj
+//
+//  Created by KahunaOSx on 11/30/16.
+//  Copyright © 2016 Kahuna Systems Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFrameworkProj.
+FOUNDATION_EXPORT double TestFrameworkProjVersionNumber;
+
+//! Project version string for TestFrameworkProj.
+FOUNDATION_EXPORT const unsigned char TestFrameworkProjVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameworkProj/PublicHeader.h>
+
+
